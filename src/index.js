9ddea51fca835d058c6ddd8f867bd1b9d482a150
SonGlad <okoshevy@gmail.com>
2024-10-08 14:55:46 +0300

    remove

--- src/index.js
+++ src/index.js
@@ -5,27 +5,20 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './globalStyles/globaleStyles.styled.js';
 import { theme } from './globalStyles/theme.js';
-// import { Provider } from 'react-redux';
-// import { PersistGate } from 'redux-persist/integration/react';
-// import {store, persistor} from "./redux/store.js";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
-  <React.StrictMode>
-    {/* <Provider store = {store}> */}
-      {/* <PersistGate loading={null} persistor={{persistor}}> */}
-        <BrowserRouter basename='/JoinToItTestTask'>
-          <ThemeProvider theme={theme}>
-            <GlobalStyle/>
-            <App />
-          </ThemeProvider>
-        </BrowserRouter>
-      {/* </PersistGate> */}
-    {/* </Provider> */}
-  </React.StrictMode>
+  // <React.StrictMode>
+    <BrowserRouter basename='/JoinToItTestTask'>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle/>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  // </React.StrictMode>
 );
 
 
